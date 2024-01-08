7323892015840106a241c0092ea9bdf67ae37c79
Coder21082003 <hbhdtr123@gmail.com>
2024-01-08 19:30:40 +0700

    first push

---
+++ lib/presentation/splash_screen/splash_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ui/core/app_export.dart';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 2), () {
+      Navigator.pushReplacementNamed(context, AppRoutes.loginScreen);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: theme.colorScheme.primary.withOpacity(1),
+        body: SizedBox(
+          width: double.maxFinite,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 5.v),
+              CustomImageView(
+                imagePath: ImageConstant.imgLogo,
+                height: 72.adaptSize,
+                width: 72.adaptSize,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
